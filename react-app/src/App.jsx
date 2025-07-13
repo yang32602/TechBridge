6a668c74d0770a389474b43455fb4984b03a5799
LezcanoG0820 <lezcanoguillermo0820@icloud.com>
2025-07-12 23:38:27 -0500

    cambios

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,32 +1,37 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { Header, Footer } from "./components";
-import { Home, Login, Register } from "./pages";
+import { Home, Login, Register, Postulantes, Contacto } from "./pages";
 import "./assets/styles.css";
 
-/*hola*/
+// Componente wrapper para usar hooks dentro del router
+const AppContent = () => {
+  const location = useLocation();
+  const hideHeaderFooter = /^\/(login|register)(\/.*)?$/.test(location.pathname);
+
+  return (
+    <div className="app">
+      {!hideHeaderFooter && <Header />}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/login/:userType" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/register/:userType" element={<Register />} />
+        <Route path="/postulantes" element={<Postulantes />} />
+        <Route path="/contacto" element={<Contacto />} />
+      </Routes>
+
+      {!hideHeaderFooter && <Footer />}
+    </div>
+  );
+};
 
 function App() {
   return (
     <Router>
-      <div className="app">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Header />
-                <Home />
-                <Footer />
-              </>
-            }
-          />
-          <Route path="/login" element={<Login />} />
-          <Route path="/login/:userType" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/register/:userType" element={<Register />} />
-        </Routes>
-      </div>
+      <AppContent />
     </Router>
   );
 }
