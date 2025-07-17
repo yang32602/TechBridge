13d27b674b289b198214d568fc8f205fb3f8ba1c
FernandoBarriosDev <fernando.barrios.sky@gmail.com>
2025-07-17 15:42:42 -0500

    api para los datos de los estudiantes
    se manejan los datos de los estudiantes como para actualizar y para extraer su informacion

--- backend/src/models/estudiantes.model.js
+++ backend/src/models/estudiantes.model.js
@@ -1,9 +1,9 @@
 import db from '../config/db.js';
 
-export const getEstudiantes = async () => {
-    const sql = 'SELECT * FROM estudiantes';
+export const getEstudiantes = async (id_usuario) => {
+    const sql = 'SELECT * FROM estudiantes WHERE id_usuario = ?';
     try {
-        const [rows] = await db.query(sql);
+        const [rows] = await db.query(sql,[id_usuario]);
         return rows;
     } catch (error) {
         console.error('Error al obtener los estudiantes:', error);
@@ -25,3 +25,26 @@ export const insertEstudiante = async (estudianteData,id_usuario) => {
         throw error;
     }
 };
+
+
+export const actualizarCampoEstudiante = async (id, campo, valor) => {
+    const camposPermitidos = [
+        'nombre_completo',
+        'fecha_nacimiento',
+        'cedula',
+        'sobremi',
+        'github',
+        'lenguajes',
+        'pais',
+        'contratado'
+    ];
+
+    if (!camposPermitidos.includes(campo)) {
+        throw new Error(`El campo "${campo}" no está permitido para actualizar.`);
+    }
+
+    const sql = `UPDATE estudiantes SET ${campo} = ? WHERE id = ?`;
+    const [resultado] = await db.query(sql, [valor, id]);
+    return resultado;
+};
+
