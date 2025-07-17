f56f895af54b4b6475c5ab29398ca146a21ca0f8
FernandoBarriosDev <fernando.barrios.sky@gmail.com>
2025-07-17 08:35:12 -0500

    restar puntos
    ahora se pueden restar los puntos a la cantidad de tokens que tiene originalmente la empresa

--- backend/src/routes/puntos.routes.js
+++ backend/src/routes/puntos.routes.js
@@ -3,6 +3,10 @@ import * as puntosController from '../controllers/puntos.controller.js';
 
 const router = express.Router();
 
+// Ruta para obtener la cantidad de puntos de un usuario
 router.post('/puntosCantidad', puntosController.cantidadPuntos);
+
+// Ruta para gastar puntos de un usuario
+router.post('/puntosGastos', puntosController.gastarPuntos);
 
 export default router;
